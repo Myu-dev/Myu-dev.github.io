5653a261c256be8c42d38d71c45af34512c25803
Myu <chitoseame.jp@gmail.com>
2026-01-11 23:30:01 +0900

    response.textをresponse.text()に修正

--- scratch/unfollower/main.js
+++ scratch/unfollower/main.js
@@ -18,7 +18,7 @@ async function nowFollower(username){
     while(hasMore){
         const response=await fetch(`https://scratch.mit.edu/users/${username}/followers/?page=${page}`);
         if(!response.ok)throw new Error("User not found");
-        const html=await response.text;
+        const html=await response.text();
         const parser=new DOMParser();
         const doc=parser.parseFromString(html);
         
