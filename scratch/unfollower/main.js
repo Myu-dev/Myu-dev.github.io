69854c4e8e08acc036666dd42b9ea60cf5aef873
Myu <chitoseame.jp@gmail.com>
2026-01-11 23:19:08 +0900

    now followerの取得

--- scratch/unfollower/main.js
+++ scratch/unfollower/main.js
@@ -1,7 +1,31 @@
 const input=document.getElementById("user-input");
 const button=document.getElementById("user-button");
+const result=document.querySelector(".result")
 let processing=false;
 button.addEventListener("click",async()=>{
     const user=input.value;
-    alert(user)
+    const unfollowers=await unfollower(username);
+    result.textContent=unfollowers;
 });
+async function unfollower(username){
+    const nowFollowers=await nowFollower(username);
+    return nowFollowers
+}
+async function nowFollower(username){
+    let page=1;
+    let hasMore=true;
+    const nowFollowers=[];
+    while(hasMore){
+        const response=await fetch(`https://scratch.mit.edu/users/${user}/followers/?page=${page}`);
+        if(!response.ok)throw new Error("User not found");
+        const html=await response.text;
+        const parser=new DOMParser();
+        const doc=parser.parseFromString(html);
+        
+        const users=[...doc.querySelectorAll("span.title > a")].map(el=>el.textContent.trim());
+        if(users.length===0)hasMore=false;
+        nowFollowers.push(...users);
+        page++;
+        if(page>1000)hasMore=false;
+    }
+}
